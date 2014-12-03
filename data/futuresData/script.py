9034edd2f74cba75d3808cd08c2ba76c4b841a6b
komal2608 <komalsnehal1991@gmail.com>
2014-12-03 02:56:49 -0500

    Improved plots, correlation, VAR

--- data/futuresData/script.py
+++ data/futuresData/script.py
@@ -1,7 +1,7 @@
 import urllib
 
-months = ['F', 'G', 'H', 'J', 'K', 'M', 'N', 'Q', 'U', 'V', 'X', 'Z',] 
-for year in range(2005,1983,-1):
+months = ['F', 'G', 'H', 'J', 'K', 'M', 'N', 'Q', 'U', 'V', 'X', 'Z'] 
+for year in range(1989,1983,-1):
 	for month in months:
 		filename = 'FUTURE_CL' + month + str(year) + '.csv'
 		url = 'https://www.quandl.com/api/v1/datasets/OFDP/FUTURE_CL' + month + str(year) + '.csv?collapse=monthly'
