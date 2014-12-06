821bab0df9e809a1d2d0000feb348606d71096ae
komal2608 <komalsnehal1991@gmail.com>
2014-12-05 21:04:00 -0500

    added gold futures data of the past 30 years

--- data/futuresDataGold/script.py
+++ data/futuresDataGold/script.py
@@ -1,12 +1,12 @@
 import urllib
 
 months = ['F', 'G', 'H', 'J', 'K', 'M', 'N', 'Q', 'U', 'V', 'X', 'Z'] 
-for year in range(2014,2009,-1):
+for year in range(2000,1984,-1):
 	for month in months:
 		#filename = 'FUTURE_CL' + month + str(year) + '.csv'
 		filename = 'CME-GC' + month + str(year) + '.csv'
 		#url = 'https://www.quandl.com/api/v1/datasets/OFDP/FUTURE_CL' + month + str(year) + '.csv?collapse=monthly'
-		url = 'https://www.quandl.com/api/v1/datasets/CME/GC' + month + str(year) + '.csv?collapse=monthly'
+		url = 'https://www.quandl.com/api/v1/datasets/CME/GC' + month + str(year) + '.csv?&auth_token=x8qkg4WAXwcJmaxx_s_u&collapse=monthly'
 		print("Downloading " + url + " to " + filename + " ...")
 		urllib.urlretrieve(url,filename)
 		print("Done!")
@@ -15,3 +15,5 @@ for year in range(2014,2009,-1):
 #APRIL: https://www.quandl.com/api/v1/datasets/CME/GCJ2014.csv?&collapse=monthly
 #MAY: https://www.quandl.com/api/v1/datasets/CME/GCK2014.csv?&collapse=monthly
 #NOVEMBER: https://www.quandl.com/api/v1/datasets/CME/GCX2014.csv?collapse=monthly
+
+#x8qkg4WAXwcJmaxx_s_u	
