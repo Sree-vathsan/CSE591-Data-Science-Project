1f850a40fbb53a1d84e1bb884350a07e1be88a8f
komal2608 <komalsnehal1991@gmail.com>
2014-12-05 20:14:05 -0500

    updated for gold

--- data/futuresData/script.py
+++ data/futuresData/script.py
@@ -1,10 +1,12 @@
 import urllib
 
 months = ['F', 'G', 'H', 'J', 'K', 'M', 'N', 'Q', 'U', 'V', 'X', 'Z'] 
-for year in range(1989,1983,-1):
+for year in range(2014,2009,-1):
 	for month in months:
-		filename = 'FUTURE_CL' + month + str(year) + '.csv'
-		url = 'https://www.quandl.com/api/v1/datasets/OFDP/FUTURE_CL' + month + str(year) + '.csv?collapse=monthly'
+		#filename = 'FUTURE_CL' + month + str(year) + '.csv'
+		filename = 'FUTURE_GC' + month + str(year) + '.csv'
+		#url = 'https://www.quandl.com/api/v1/datasets/OFDP/FUTURE_CL' + month + str(year) + '.csv?collapse=monthly'
+		url = 'https://www.quandl.com/api/v1/datasets/OFDP/FUTURE_GC' + month + str(year) + '.csv?collapse=monthly'
 		print("Downloading " + url + " to " + filename + " ...")
 		urllib.urlretrieve(url,filename)
 		print("Done!")
